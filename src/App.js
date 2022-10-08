663aad60fceab66b1d0e335aff494f241a472c6a
samahalaymun <samah_abu_laymun@hotmail.com>
2022-10-08 19:12:42 +0300

    fix bugs2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes ,Route,HashRouter } from 'react-router-dom'
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import  Details  from './components/pages/Details';
@@ -9,15 +9,13 @@ import { useContext } from 'react';
 function App() {
   const {theme} = useContext(Context)
   return (
-    <Box>
-    <Router basename="/">
+    <HashRouter>
      <Header />
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:country" element={<Details />} />
       </Routes>
-    </Router>
-    </Box>
+    </HashRouter>
   );
 }
 
